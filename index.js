3c0c75de6283d223114ebabaf2223bbdd22ecada
Cata <129232492+CatGonzal@users.noreply.github.com>
2023-04-20 16:51:06 -0300

    Update and rename README.md to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Cata",
+  22,
+  true,
+  new Date(2001, 02, 11),
+  { 
+      titulo: "El Poder de la Conciencia (Spanish Edition) ",
+      autor: "Neville Goddard",
+      fecha: new Date(2018, 11, 20),
+      url: "https://www.amazon.com/-/es/Neville-Goddard/dp/1731339828"
+    },
+];
